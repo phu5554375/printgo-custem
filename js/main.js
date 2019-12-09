50f71de84e0068fe7a620b3ac81b9f7301dac117
phu5554375 <phu5554375@gmail.com>
2019-12-09 18:19:35 +0700

    uppp

--- js/main.js
+++ js/main.js
@@ -180,6 +180,28 @@ $(document).ready(function () {
             }
         }
     });
+    $('#singleProductPhotoCarousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
     $('#productCarousel').owlCarousel({
         loop: true,
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
