e786133214e863c6abd7a3e1446b0a9e000afcba
phu5554375 <phu5554375@gmail.com>
2020-01-09 17:23:20 +0700

    uppp

--- js/main.js
+++ js/main.js
@@ -196,7 +196,7 @@ $(document).ready(function () {
                 nav: false
             },
             1000: {
-                items: 3,
+                items: 4,
                 nav: true,
                 loop: false
             }
